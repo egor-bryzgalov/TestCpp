bafdb5ba20556e25064e9b7694fa207399582242
Egor Bryzgalov <v-egbryz@microsoft.com>
2020-05-11 13:09:49 +0300

    Added files

---
+++ CPPTest/CPPTest.cpp
@@ -0,0 +1,15 @@
+﻿// CPPTest.cpp : Defines the entry point for the application.
+//
+
+#include "CPPTest.h"
+
+using namespace std;
+
+int main()
+{
+	int64_t A[10];
+	int64_t* p = A;
+	*(p + 1) = 5;
+	cout << A[1] << endl;
+	return 0;
+}
